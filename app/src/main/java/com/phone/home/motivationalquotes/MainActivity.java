9f23f6edb8d242e51b6d725a3fe104f9d8f964be
TiahmSotoudeh <sotoudehtiahm@gmail.com>
2016-11-21 15:44:24 -0800

    forgot curly braces lmao

--- app/src/main/java/com/phone/home/motivationalquotes/MainActivity.java
+++ app/src/main/java/com/phone/home/motivationalquotes/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity {
 
     int memes = 0;
 

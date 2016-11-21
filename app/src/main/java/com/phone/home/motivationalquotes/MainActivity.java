48e2dc0b7e95076faea50bdcf01042c5b79085e9
TiahmSotoudeh <sotoudehtiahm@gmail.com>
2016-11-21 15:37:12 -0800

    WE DID IT BOYS

---
+++ app/src/main/java/com/phone/home/motivationalquotes/MainActivity.java
@@ -0,0 +1,35 @@
+package com.phone.home.motivationalquotes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+
+    int memes = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "ALOHA", Snackbar.LENGTH_LONG)
+                        .setAction(String.valueOf(memes), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                memes++;
+                            }
+                        }).show();
+            }
+        });
+    }
+}

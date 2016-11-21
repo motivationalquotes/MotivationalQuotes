6bf71e84340e1008b2beb60b5ccf96f5a3c54cb8
TiahmSotoudeh <sotoudehtiahm@gmail.com>
2016-11-21 14:55:14 -0800

    first commit i think

---
+++ README.md
@@ -0,0 +1,2 @@
+# Motivational Quote App
+Pulls motivational quotes from a database, and helps set a goal to apply the quote.
---
+++ app/src/main/java/com/phone/home/motivationalquotes/MainPage.java
@@ -0,0 +1,56 @@
+package com.phone.home.motivationalquotes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        int numberOfMemes = 0;
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Aloha " + numberOfMemes, Snackbar.LENGTH_LONG).setAction("Snackbar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        numberOfMemes++;
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

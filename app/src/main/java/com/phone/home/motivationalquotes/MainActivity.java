1e4e6daea9a280cc03c840c7c7280bce8be36a47
TiahmSotoudeh <sotoudehtiahm@gmail.com>
2016-11-22 01:37:03 -0800

    different icons, start notification functionality

--- app/src/main/java/com/phone/home/motivationalquotes/MainActivity.java
+++ app/src/main/java/com/phone/home/motivationalquotes/MainActivity.java
@@ -1,14 +1,12 @@
 package com.phone.home.motivationalquotes;
 
-import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
-import android.view.Gravity;
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -18,18 +16,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         constructQuoteArray(quoteList);
+        Scanner awhdhioawdho = new Scanner();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setImageResource(R.drawable.ic_add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar snackbar = Snackbar.make(view, quoteList.get((int) (Math.random() * quoteList.size())), Snackbar.LENGTH_SHORT);
-
 
                 snackbar.show();
             }
